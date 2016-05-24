d5ef69e9065b4b90417d9666f994b4d3f73e23ab
Tan Ping <tan.ping@vpclub.cn>
2016-05-24 09:45:42 +0800

    ssd

---
+++ OkHttpDemo/src/com/vpnet/VpResponse.java
@@ -0,0 +1,13 @@
+package com.vpnet;
+
+import java.io.Serializable;
+
+import okhttp3.Headers;
+
+public class VpResponse implements Serializable {
+	
+	public String errorMsg;
+	public String body;
+	public Headers headers;
+	public VpCallBack callBack;
+}
